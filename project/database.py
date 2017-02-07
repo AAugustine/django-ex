1ce77994e523925dc579c2146c64ff6dc86e9215
Alexandra Augustine <aaugusti@aaugusti.rdu.csb>
2017-02-07 11:03:50 -0500

    Remove welcome files

--- project/database.py
+++ project/database.py
@@ -1,29 +1,22 @@
-import os
-
 from django.conf import settings
 
 
-engines = {
-    'sqlite': 'django.db.backends.sqlite3',
-    'postgresql': 'django.db.backends.postgresql_psycopg2',
-    'mysql': 'django.db.backends.mysql',
-}
-
-
-def config():
-    service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
-    if service_name:
-        engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
+def info():
+    db_settings = settings.DATABASES['default']
+    if 'postgres' in db_settings['ENGINE']:
+        engine = 'PostgreSQL'
+        url = '{HOST}:{PORT}/{NAME}'.format(**db_settings)
+    elif 'mysql' in db_settings['ENGINE']:
+        engine = 'MySQL'
+        url = '{HOST}:{PORT}/{NAME}'.format(**db_settings)
+    elif 'sqlite' in db_settings['ENGINE']:
+        engine = 'SQLite'
+        url = '{NAME}'.format(**db_settings)
     else:
-        engine = engines['sqlite']
-    name = os.getenv('DATABASE_NAME')
-    if not name and engine == engines['sqlite']:
-        name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
+        engine = 'unknown'
+        url = ''
     return {
-        'ENGINE': engine,
-        'NAME': name,
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
-        'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
+        'engine': engine,
+        'url': url,
+        'is_sqlite': engine == 'SQLite',
     }
