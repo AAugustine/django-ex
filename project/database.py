9b9e9f761673e33db13d651eef6d10d2ca56a7f7
Alexandra Augustine <aaugusti@aaugusti.rdu.csb>
2017-02-07 11:51:22 -0500

    Fixed my db mistake

--- project/database.py
+++ project/database.py
@@ -1,22 +1,32 @@
 from django.conf import settings
 
 
-def info():
-    db_settings = settings.DATABASES['default']
-    if 'postgres' in db_settings['ENGINE']:
-        engine = 'PostgreSQL'
-        url = '{HOST}:{PORT}/{NAME}'.format(**db_settings)
-    elif 'mysql' in db_settings['ENGINE']:
-        engine = 'MySQL'
-        url = '{HOST}:{PORT}/{NAME}'.format(**db_settings)
-    elif 'sqlite' in db_settings['ENGINE']:
-        engine = 'SQLite'
-        url = '{NAME}'.format(**db_settings)
+import os
+
+from django.conf import settings
+
+
+engines = {
+    'sqlite': 'django.db.backends.sqlite3',
+    'postgresql': 'django.db.backends.postgresql_psycopg2',
+    'mysql': 'django.db.backends.mysql',
+}
+
+
+def config():
+    service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper()
+    if service_name:
+        engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
     else:
-        engine = 'unknown'
-        url = ''
+        engine = engines['sqlite']
+    name = os.getenv('DATABASE_NAME')
+    if not name and engine == engines['sqlite']:
+        name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
     return {
-        'engine': engine,
-        'url': url,
-        'is_sqlite': engine == 'SQLite',
+        'ENGINE': engine,
+        'NAME': name,
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
+        'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
     }
