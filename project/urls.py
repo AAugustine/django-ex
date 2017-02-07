25cb8798402d3f5ebb6867cd04b72fed6ead22f7
Alexandra Augustine <aaugusti@aaugusti.rdu.csb>
2017-02-07 10:44:06 -0500

    Simple Python API tutorial

--- project/urls.py
+++ project/urls.py
@@ -8,7 +8,13 @@ urlpatterns = [
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', index),
-    url(r'^health$', health),
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^$', index),
+    # url(r'^health$', health),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('snippets.urls')),
+    
+]
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
